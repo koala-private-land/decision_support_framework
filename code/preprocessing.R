6df970999330d8149a02b5c190df4eb7b0d36a43
Brooke Williams <uqbwil14@uq.edu.au>
2022-03-24 17:08:46 +1000

    Updates to code - have included all proability data (from predictions) and conservation benefit now represents area of koala habitat

--- code/preprocessing.R
+++ code/preprocessing.R
@@ -8,40 +8,84 @@ library(foreign)
 library(spatialEco)
 library(terra)
 library(rgeos)
+library(gurobi)
+library(exactextractr)
 
 setwd("D:/Linkage/DSF code/private_land_conservation_DSF/")
 
 #Ranking
 #The formula for ranking is 
-
 #((condition*status*0.8)+(connectivity*proximity to sites*proximity to PAs*0.2))*duration(1)*risk*area
 
-#Read in the property shp file
-properties <- readOGR("./raw_data/Properties/Properties.shp")
-#Import koala liklihood layer
-KML <- readOGR("D:/Linkage/DSF code/private_land_conservation_DSF/raw_data/Biodiversity_KLM_v2.0_August_2019/KLM_v2.0_August_2019.shp")
-crs <- crs(KML)
-properties <- sp::spTransform(properties, crs(crs))
-save(properties, file = "./preprocessing/properties_proj.RData")
-#Load projected property layer
-load("./preprocessing/properties_proj.RData")
+# # The input file geodatabase
+#fgdb <- "D:/Linkage/Data/Study region/properties1.gdb"
+# # List all feature classes in a file geodatabase
+#subset(ogrDrivers(), grepl("GDB", name))
+#fc_list <- ogrListLayers(fgdb)
+#print(fc_list)
+# # Read the feature class
+#properties <- readOGR(dsn=fgdb,layer="lots_mosaic_final_private_2ha_not_intense_not_water")
+# # Determine the FC extent, projection, and attribute information
+#summary(properties)
+# # View the feature class
+#plot(properties)
 
-#Get the coordiates of the polygons
+#Load dissolved property layer
+properties <- readOGR("./preprocessing/Properties_dissolved.shp")
+crs <- proj4string(properties)
+save(properties, file = "./preprocessing/properties.RData")
+load("./preprocessing/properties.RData")
+
+#Read in the property shp file
+#properties <- readOGR("./raw_data/Properties/Properties.shp")
+#Import koala liklihood layer
+# KML <- readOGR("D:/Linkage/DSF code/private_land_conservation_DSF/raw_data/Biodiversity_KLM_v2.0_August_2019/KLM_v2.0_August_2019.shp")
+# save(KML, file = "./preprocessing/KML.RData")
+# load("./preprocessing/KML.RData")
+# crs <- crs(KML)
+
+#Get Koala likelihood
+# pnts_KML <- over(points, KML)
+# save(pnts_KML, file="./preprocessing/pnts_KML.RData")
+# load("./preprocessing/pnts_KML.RData")
+# pnts_KML_prob <- pnts_KML$p
+#Get area of Koala habitat
+
+#Import koala habitat layer
+#khab <- raster("./raw_data/khab_thresh.tif")
+#Need to create an area raster
+#cell_size <- area(khab, na.rm=TRUE, weights=FALSE)
+#writeRaster(cell_size, './preprocessing/cell_size.tif')
+#khab_zonal <- zonal.stats(properties, khab, stats="sum")
+#save(khab_zonal, file="./preprocessing/khab_zonal.RData")
+#load("./preprocessing/khab_zonal.RData")
+
+#Had to do this zonal statistics in Arcmap - R couldn't handle it
+koala <- read.csv("./preprocessing/propid_koalahabitat.csv")
+
+#properties <- sp::spTransform(properties, crs(crs))
+#save(properties, file = "./preprocessing/properties_proj.RData")
+#Load projected property layer
+#load("./preprocessing/properties_proj.RData")
+
+#Get the coordinates of the polygons
 coords <- coordinates(properties)
 save(coords, file="./preprocessing/coords.RData")
 load("./preprocessing/coords.RData")
+
 #Get puid
 points <- SpatialPoints(coords, proj4string=crs(crs))
+proj4string(points) <- crs
+proj4string(properties) <- crs
 pnts_properties <- over(points, properties)
 save(pnts_properties, file="./preprocessing/pnts_properties.RData")
+load("./preprocessing/pnts_properties.RData")
 head(pnts_properties)
-puid <- pnts_properties$gurasid
+
+propid <- pnts_properties$propid
 #Get area - note are in shp file has to be correct, check first in ArcMap
-area <- pnts_properties$Shape__Are
-#Get Koala likelihood
-pnts_KML <- over(points, KML)
-save(pnts_KML, file="./preprocessing/pnts_KML.RData")
-pnts_KML_prob <- pnts_KML$p
+#This is m2
+area <- pnts_properties$Shape_Area
 
 #Condition
 #Just using a condition layer for now
@@ -67,6 +111,9 @@ cond_rast <- raster("./raw_data/bba_31a_cond_V2_nsw_raw.tif")
 cond <- zonal.stats(properties, cond_rast, stats="mean")
 #Could be faster
 #cond <- extract(cond_rast, properties, "mean") 
+cond <- data.frame(properties$propid, cond)
+save(cond, file="./preprocessing/cond.RData")
+load("./preprocessing/cond.RData")
 
 ##Connectivity
 connect_rast <- raster("./raw_data/bba_31b_connectivity_V2_nsw_raw.tif")
@@ -76,12 +123,12 @@ connect_rast <- raster("./raw_data/bba_31b_connectivity_V2_nsw_raw.tif")
 #have to do this on either a server or in QGIS
 #properties_buff_1500m <- gBuffer(properties, width=1500) 
 properties_buff_1500m <- readOGR("./preprocessing/Properties_buffered.shp")
-properties_buff_1500m <- sp::spTransform(properties_buff_1500m, crs(crs))
-save(properties_buff_1500m, file = "./preprocessing/properties_buff_proj.RData")
-load("./preprocessing/properties_buff_proj.RData")
-#connect <- extract(connect_rast, properties, "mean") 
-connect <- zonal.stats(properties_buff_1500m, connect_rast, stats="mean")
 
+connect <- zonal.stats(properties_buff_1500m, connect_rast, stats="mean")
+#connect <- extract(connect_rast, properties_buff_1500m, fun='mean') 
+connect <- data.frame(properties_buff_1500m$propid, connect)
+save(connect, file="./preprocessing/connect.RData")
+load("./preprocessing/connect.RData")
 
 ##Proximity to nearby sites
 cov <- readOGR("./preprocessing/All_BCT_agreements_merged/All_BCT_agreements_merged.shp")
@@ -95,9 +142,13 @@ r1 <- rasterize(cov, r, 1)
 r2 <- mask(r1, r1, maskvalue=1, inverse=TRUE, updatevalue=NA)
 dist_cov <- distance(r2)
 #crs = crs(connect_rast)
-dist_cov <- projectRaster(dist_cov, crs = crs)
+dist_cov <- projectRaster(dist_cov, crs = crs(properties_buff_1500m))
 writeRaster(dist_cov,'./preprocessing/dist_cov_m.tif', overwrite=TRUE)
+dist_cov <- raster("./preprocessing/dist_cov_m.tif")
 dist_cov_prop <- zonal.stats(properties, dist_cov, stats="mean")
+dist_cov_prop <- data.frame(properties$propid, dist_cov_prop)
+save(dist_cov_prop, file="./preprocessing/dist_cov_prop.RData")
+load("./preprocessing/dist_cov_prop.RData")
 
 ##Proximity to protected areas
 #https://stackoverflow.com/questions/35555709/global-raster-of-geographic-distances
@@ -107,12 +158,15 @@ r <- raster(xmn=15696293, xmx=17100107, ymn=-4473529, ymx=-3279325, res=1000, cr
 r1 <- rasterize(pa, r, 1)
 r2 <- mask(r1, r1, maskvalue=1, inverse=TRUE, updatevalue=NA)
 dist_pa <- distance(r2)
-dist_pa <- projectRaster(dist_pa, crs = crs)
+dist_pa <- projectRaster(dist_pa, crs = crs(properties_buff_1500m))
 writeRaster(dist_pa,'./preprocessing/dist_pa_m.tif', overwrite=TRUE)
+dist_pa <- raster("./preprocessing/dist_pa_m.tif") 
 dist_pa_prop <- zonal.stats(properties, dist_pa, stats="mean")
+dist_pa_prop <- data.frame(properties$propid, dist_pa_prop)
+save(dist_pa_prop, file="./preprocessing/dist_pa_prop.RData")
+load("./preprocessing/dist_pa_prop.RData")
 
 #Risk
-#Somethin
 load("./preprocessing/coords.RData")
 risk <- readOGR("./raw_data/Land and Soil Capability Mapping for NSW/LSC_MstLmt_allHzd_NSW_v4_200923_proj.shp")
 points <- SpatialPoints(coords, proj4string=crs(risk))
@@ -130,52 +184,111 @@ pnts_risk_v3[pnts_risk_v3 == 500] <- 2
 pnts_risk_v3[pnts_risk_v3 == 600] <- 2
 pnts_risk_v3[pnts_risk_v3 == 700] <- 1
 pnts_risk_v3[pnts_risk_v3 == 800] <- 1
+save(pnts_risk_v3, file="./preprocessing/pnts_risk_v3.RData")
+load("./preprocessing/pnts_risk_v3.RData")
 #pu.df <- cbind(pu.df, pnts_risk$Label)
 
-#Tie it together in a dataframe
-pu.df <- data.frame(puid=puid, area=area, kml=pnts_KML_prob, cond=cond, connect=connect, risk = pnts_risk_v3, dist_cov = dist_cov_prop, dist_pa = dist_pa_prop)
-colnames(pu.df) <- c("puid", "area", "kml", "cond", "connect", "risk", "dist_cov", "dist_pa")
+
+#Some of this can be tied together and some of it has to be merged
+pu.df <- data.frame(propid=propid, area=area, risk = pnts_risk_v3)
+colnames(pu.df) <- c("propid", "area", "risk")
+pu.df = pu.df[pu.df$propid > 0,]
+
+#cond
+colnames(cond) <- c("propid", "cond")
+cond = cond[cond$propid > 0,]
+merged <- merge(pu.df, cond, by='propid')
+#connect
+colnames(connect) <- c("propid", "connect")
+connect = connect[connect$propid > 0,]
+merged <- merge(merged, connect, by='propid')
+#dist_cov_prop
+colnames(dist_cov_prop) <- c("propid", "dist_cov")
+merged <- merge(merged, dist_cov_prop, by='propid')
+#dist_pa_prop
+colnames(dist_pa_prop) <- c("propid", "dist_pa")
+merged <- merge(merged, dist_pa_prop, by='propid')
+
+#pu.df <- merged[c("propid", "area", "cond.x", "connect.x", "risk", "dist_cov", "dist_pa",  cond=risk = pnts_risk_v3, dist_cov = dist_cov_prop, dist_pa = dist_pa_prop)]
+colnames(pu.df) <- c("propid", "area", "risk", "cond", "connect", "dist_cov", "dist_pa")
+
+###Need to add LGA. 
+properties.dbf <- read.dbf("./preprocessing/LGA_properties_intersection_CADID_retained.dbf", as.is = FALSE)
+#Pull out just LGA's
+properties.dbf_LGA <- properties.dbf[c("propid", "CADID")]
+#Remove duplicates
+properties.dbf_LGA <- properties.dbf_LGA[!duplicated(properties.dbf_LGA), ]
+merged <- merge(merged, properties.dbf_LGA, by='propid')
+pu.df <- merged[c("CADID", "propid", "area", "cond", "connect", "risk", "dist_cov", "dist_pa")]
+
+##Up to here, redoing the Koala values for the dissolved layer
+##Add the koala/conservation values
+merged <- merge(pu.df, koala, by='propid')
+pu.df <- merged[c("CADID", "propid", "area", "cond", "connect", "risk", "dist_cov", "dist_pa", "k_area")]
+colnames(pu.df) <- c("LGA", "propid", "area", "cond", "connect", "risk", "dist_cov", "dist_pa", "koala")
 
 #Do the ranking formula
+pu.df$rank <- (pu.df$cond*pu.df$koala*0.8)+((pu.df$connect+pu.df$dist_cov+pu.df$dist_pa)*0.2)*1*pu.df$risk*pu.df$area
 
-pu.df$rank <- (pu.df$cond*pu.df$kml*0.8)+((pu.df$connect+pu.df$dist_cov+pu.df$dist_pa)*0.2)*1*pu.df$risk*pu.df$area
+#Bring in the probability data
+#Intersect the property data with the LGA's - needed to do this in QGis, couldn't get it working in R
+#LGA_prop_intersect <- readOGR("./preprocessing/LGA_properties_intersection_CADID_retained.shp")
+#Just for the table
+LGA_prop_intersect.dbf <- read.dbf("./preprocessing/LGA_properties_intersection_CADID_retained.dbf", as.is = FALSE)
+colnames(LGA_prop_intersect.dbf)[3] <- "CADID_LGA"
+colnames(LGA_prop_intersect.dbf)[21] <- "CADID"
+#properties.dbf_cost <- properties.dbf[c("propid", "LValHa")]
+#properties.dbf_ag <- aggregate(properties.dbf_cost, by = list(properties.dbf$propid), FUN = "sum")
+
+#Join property data with probability predictions
+probs <- read.csv("D:/Linkage/DSF code/private_land_conservation_DSF/raw_data/Probability/spatial_predictions_inf.csv")
+properties_prob <- merge(LGA_prop_intersect.dbf, probs, by='CADID')
+####Need to turn bid price into total. WTA == $1000/ha/yr 
+properties_prob$MeanWTA.tot <- properties_prob$Area_H*properties_prob$MeanProp*properties_prob$MeanWTA*1000
+##All of this aggregating won't be necessary with Jonathan's new predictions which are at the "propid" level
+MeanWTA.tot <- aggregate(x = properties_prob[c("MeanWTA.tot")], by = properties_prob[c("propid")], FUN = sum)
+MeanWTA.tot <- MeanWTA.tot[MeanWTA.tot$propid > 0,]
+properties_prob.ag <- aggregate(x = properties_prob[c("MeanAdopt", "MeanProp")], by = properties_prob[c("propid")], FUN = mean)
+properties_prob.ag <- properties_prob.ag[properties_prob.ag$propid > 0,]
+merged <- merge(properties_prob.ag, MeanWTA.tot, by='propid')
+
+#Merge with main dataframe
+merged <- merge(pu.df, merged, by='propid')
+pu.df <- merged[c("LGA", "propid", "area", "cond", "connect", "risk", "dist_cov", "dist_pa", "koala", "rank","MeanAdopt", "MeanWTA.tot", "MeanProp")]
+colnames(pu.df) <- c("LGA", "propid", "area", "cond", "connect", "risk", "dist_cov", "dist_pa", "koala", "rank", "prob.property", "bid.price", "MeanProp")
+pu.df$koala.w <- pu.df$koala*pu.df$MeanProp
+
+#checks
+#This should be zero
+list <- which(pu.df$area < pu.df$koala.w)
+list
+
+##Need to estimate the amount that would realistically be spent on each tender
+cost <- read.csv("./raw_data/bct_agreement_cost.csv")
+#Get total values of conservation tenders
+cost$mintot <- cost$total_area*cost$min_cost_ha
+cost$maxtot <- cost$total_area*cost$max_cost_ha
+
+length(unique(pu.df$LGA))
+lga <- unique(pu.df$LGA)
+npv <- rep(c(runif(c(1:99), min=mean(cost$mintot), max=mean(cost$maxtot))))
+npv.df <- data.frame(LGA=lga, NPV=npv)
+merged <- merge(pu.df, npv.df, by='LGA')
+merged$admin <- 1
+pu.df <- merged
 
 save(pu.df, file="./preprocessing/pu.df.RData")
 load("./preprocessing/pu.df.RData")
 
-#pu.df <- cbind(pu.df, dist_cov_prop)
+#Make the final df for the optimisation
 
-# #Pull out the LGAs
-# # The input file geodatabase
-# fgdb <- "./Study region/project_boundaries/project_boundaries.gdb"
-# # List all feature classes in a file geodatabase
-# subset(ogrDrivers(), grepl("GDB", name))
-# fc_list <- ogrListLayers(fgdb)
-# print(fc_list)
-# # Read the feature class
-# lgas <- readOGR(dsn=fgdb,layer="nsw_lga_subset_GDA2020_all")
-# # Determine the FC extent, projection, and attribute information
-# summary(lgas)
-# # View the feature class
-# plot(lgas)
-# crs <- proj4string(lgas)
-# 
-# #Import property data
-# properties <- rgdal::readOGR("./Study region/Properties/properties_mosaic_final_private_2ha_not_intense_not_water.shp")
-# proj4string(properties) <- CRS(crs)
-# #plot(properties, add = TRUE)
-# #properties <- spTransform(properties, crs)
-#Add LGA ID to the property data
-#intersect_lga_prop <- gIntersects(lgas, properties_prob, byid = TRUE)
-#properties_lgas <- st_join(properties_prob, left = FALSE, lgas["LG_PLY_PID"]) # join points
+df <- pu.df[c("LGA", "NPV", "admin", "rank", "prob.property", "bid.price", "koala.w")]
+save(df, file="./preprocessing/df.RData")
 
-#Intersect the property data with the LGA's - needed to do this in QGis, couldn't get it working in R
-properties <- readOGR("./Study region/LGA_properties_intersection/LGA_properties_intersection.shp")
-#Just for the table
-properties.dbf <- read.dbf("./Study region/LGA_properties_intersection/LGA_properties_intersection.dbf", as.is = FALSE)
-#Join property data with probability predictions
-probs <- read.csv("D:/Linkage/DSF code/private_land_conservation_DSF/raw_data/Probability/spatial_predictions_10yr.csv")
-properties_prob <- merge(properties.dbf, probs, by='CADID')
+
+
+
+
 
 
 
--- code/working_code.R
+++ code/working_code.R
@@ -1,36 +1,61 @@
 library(gurobi)
 library(dplyr)
+library(OpenStreetMap)
+library(ggplot2)
+library (rgdal)
+library (rgeos)
+library(maptools)
 
-#test dataset
-#Each planning unit is an LGA
-puid <- rep(c(1:20),each= 10)
-npu <- length(puid)
-#Ci is the Net present value of amount invested in a tender in planning unit i 
-npv <- rep(c(runif(c(1:20), min=500, max=8000)),each= 10)
-#Ai is the administrative costs of running a tender in planning unit i. 
-admin.cost <- rep(runif(c(1:20), min=5, max=30), each = 10)
-#property id - assumed to be in order of BCT ranking process
-property <- c(1:npu)
-#Iijk Bernoulli distributed probability that property j in planning unit i will put in a bid
-prob.property <- runif(npu, min=0, max=1)
-#pij net present value of the bid price of property j in planning unit i
-bid.price <- runif(npu, min=0, max=2000)
-#bij conservation benefit of each property
-cons.benefit <- runif(npu, min=0, max=3000)
-#Area in ha
-area <- runif(npu, min=0, max=150)
-#Tie it together to create dataframe
-df <- data.frame(puid, npv, admin.cost, property, prob.property, bid.price, cons.benefit)
-head(df)
+# #test dataset
+# #Each planning unit is an LGA
+# puid <- rep(c(1:20),each= 10)
+# npu <- length(puid)
+# #Ci is the Net present value of amount invested in a tender in planning unit i
+# npv <- rep(c(runif(c(1:20), min=500, max=8000)),each= 10)
+# #Ai is the administrative costs of running a tender in planning unit i.
+# admin.cost <- rep(runif(c(1:20), min=5, max=30), each = 10)
+# #property id - assumed to be in order of BCT ranking process
+# property <- c(1:npu)
+# #Iijk Bernoulli distributed probability that property j in planning unit i will put in a bid
+# prob.property <- runif(npu, min=0, max=1)
+# #pij net present value of the bid price of property j in planning unit i
+# bid.price <- runif(npu, min=0, max=2000)
+# #bij conservation benefit of each property
+# cons.benefit <- runif(npu, min=0, max=3000)
+# #Area in ha
+# area <- runif(npu, min=0, max=150)
+# #Tie it together to create dataframe
+# df <- data.frame(puid, npv, admin.cost, property, prob.property, bid.price, cons.benefit)
+# head(df)
+
+#Remove duplicates -> need to look into why there are duplicates
+load("./preprocessing/df.RData")
+duplicates <- df[duplicated(df), ]
+df <- df[!duplicated(df), ]
+
+#Rename the columns if need be
+#Columns must be named
+#puid
+#npv
+#admin.cost
+#propid
+#property
+#prob.property
+#bid.price
+#cons.benefit
+
+colnames(df) <- c("puid", "npv", "admin.cost", "property", "prob.property", "bid.price", "cons.benefit")
+#df <- data.frame(puid = pu.df$LGA, npv = pu.df$NPV, admin.cost = pu.df$admin, property = pu.df$rank, prob.property = pu.df$MeanAdopt, bid.price = pu.df$bid.price, cons.benefit = pu.df$koala.w)
 
 
 #Functions
 ####Function to select properties in order up until the NPV constraint
 properties <- function(df){
 df_new <- data.frame()
-for (i in 1:length(unique(df$puid))){
+for (i in unique(df$puid)){
+  #for (i in df$puid){
   df1 <- df[which(df$puid == paste0(i)),] 
-  # #####Constrint to ensure that the selected properties do not exceed the allocated funding (F)
+  # #####Constrint to ensure that the selected properties do not exceed the allocated funding for each LGA (NPV)
   if (sum(df1$bid.price) > df1[1,]$npv - df1[1,]$admin.cost) {
     #Place in the order reflecting the BCT prioritisation process
     df1 <- df1[order(df1$property),]
@@ -39,10 +64,9 @@ for (i in 1:length(unique(df$puid))){
     #Turn into a dataframe
     df1 <- as.data.frame(df1)
     #Create a subset which is only those properties lower than the investment amount for the LGA
-    df1 <- df1[which(df1$csum < df1[1,]$npv - df1[1,]$admin.cost),]    
-    #Remove the cummlative sum column
-    df1 <- df1[1:ncol(df1)-1]   
-    df1
+    df1 <- df1[which(df1$csum < df1[1,]$npv - df1[1,]$admin.cost),]
+    #remove cummulative sum column 
+    df1 <- df1[-8]
   } else{
     df1 <- df1
   }
@@ -53,13 +77,14 @@ return(df_new)
 
 df_new <- properties(df)  
 head(df_new)
+View(df_new)
 
 #Get the overall mean for each planning unit/LGA
 df_new.ag <- aggregate(x = df_new[c("npv","admin.cost", "prob.property","bid.price","cons.benefit")], by = df_new[c("puid")], FUN = mean)
 head(df_new.ag)
 
 #F represents the overall funding
-F <- 10000
+F <- 10000000
 
 #Set up the optimisation
 #Select the planning units (LGA's)
@@ -85,147 +110,26 @@ solution <- data.frame(df_new.ag$puid, result$x)
 selected_pus <- solution$df_new.ag.puid[solution$result.x==1]
 selected_pus
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-######Old Code##############
-#S represents the number of simulations
-S<- 8
-#Function to select random properties among the LGAs with budget constraint
-#The assumption here is that the order of the property ids needs to match the priority of BCT
-random.properties <- function(df, S){
-  if (S > length(unique(df$puid))){
-    print("Random selection is greater than number of properties in LGA, choose a lower number")} else {
-      df_new <- data.frame()
-      for (i in 1:length(unique(df$puid))){
-        df1 <- df[which(df$puid == paste0(i)),] 
-        #Select random sample
-        df1 <- df1[sample(nrow(df1), S), ]
-        
-        # #####Constrint to ensure that the selected properties do not exceed the allocated funding (F)
-        if (sum(df1$bid.price) > df1[1,]$npv - df1[1,]$admin.cost) {
-          #Place in the order reflecting the BCT prioritisation process
-          df1 <- df1[order(df1$property),]
-          #Create a new column that is the cummulative sum of bid price
-          df1 <- df1 %>% group_by(puid) %>% mutate(csum = cumsum(df1$bid.price))
-          #Turn into a dataframe
-          df1 <- as.data.frame(df1)
-          #Create a subset which is only those properties lower than the investment amount for the LGA
-          df1 <- df1[which(df1$csum < df1[1,]$npv - df1[1,]$admin.cost),]    
-          #Remove the cummlative sum column
-          df1 <- df1[1:ncol(df1)-1]   
-          df1
-        } else{
-          next
-        }
-        
-        df_new <- rbind(df_new, df1)
-      }
-      return(df_new)
-    }
+###Create code to export maps
+setwd("D:/Linkage/DSF code/private_land_conservation_DSF/")
+# create folder:
+if (!dir.exists("outfoldermaps")){
+  dir.create("outfoldermaps", recursive = TRUE)
+} else {
+  print("Dir already exists!")
 }
 
-#one simulated dataframe
-sim.df <- random.properties(df, 6)
-#simulated dataframe n = number of simulations
-sim.df <- replicate(n=1000, random.properties(df, 6))
-#Check matrix
-sim.df
-#Get the mean of each simulation for each planning unit
-mean.sim <- data.frame()
-for (i in 1:ncol(sim.df)){
-  ag.test <- aggregate(x = sim.df[,3][c("npv","admin.cost", "prob.property","bid.price","cons.benefit")], by = sim.df[,3][c("puid")], FUN = mean)
-  mean.sim <- rbind(mean.sim, ag.test)
-}
-#Get the overall mean for each planning unit
-sim.df.ag <- aggregate(x = mean.sim[c("npv","admin.cost", "prob.property","bid.price","cons.benefit")], by = mean.sim[c("puid")], FUN = mean)
+shp.pu <- readOGR("./raw_data/LGAs_study_region.shp")
+# plot(shp.pu)
+# shp.pu.sub <- shp.pu[shp.pu$CADID %in% c(selected_pus),]
+# plot(shp.pu.sub, col = "lightslateblue", add = TRUE)
 
-
-
-#Function to select random properties among the LGAs
-# random.properties <- function(df, S){
-#   df_new <- data.frame()
-#   for (i in 1:length(unique(df$puid))){
-#     df1 <- df[which(df$puid == paste0(i)),] 
-#     #Select random sample
-#     df1 <- df1[sample(nrow(df1), S), ]
-#     #Join the dataframe with all of the selected properties together
-#       df_new <- rbind(df_new, df1)
-#   }
-#   df_new
-# }
-# 
-# random.properties(df, 2)
-
-
-
-# #   #Remove rows from the end until it is under this ammount
-#    for (m in 1:nrow(df1)){
-#      if (sum(df1$npv[1:m]-df1$admin.cost[1:m]) > B){
-#        df1.const <- df1[1:m-1,]
-#        df_new <- rbind(df_new, df1.const)
-
-benefits <- list()
-for (i in 1:length(unique(df$puid))){
-  
-  df1 <- df[which(df$puid == paste0(i)),] 
-  
-  #Select random sample
-  df1 <- df1[sample(nrow(df1), S), ]
-  
-  #The inequality here ensured they don’t spend more than the money they have. That is they choose the maximum number of properties that they invest in so that the cost is still less than the budget, C. 
-  constr.matrix <- matrix(df1$npv - df1$admin.cost, nrow=1)
-  
-  model <- list()
-  model$A <- constr.matrix
-  model$obj        <- df1$prob.property*df1$bid.price
-  model$modelsense <- 'max'
-  model$rhs        <- F
-  model$sense      <- '<'
-  model$vtype      <- 'B'
-  
-  params <- list(OutputFlag=0)
-  
-  result <- gurobi(model, params)
-  
-  print('Solution:')
-  print(result$objval)
-  print(result$x)
-  
-  benefits[[length(benefits) + 1]] <- result$objval/S
-  
-}
-
-benefits
-
-#BEN is the expected benefit when implementing a tender in planning unit i,
-df$BEN <- rep(c(benefits),each= 10)
-
-benefits <- list()
-objective.function <- 
+png(file=paste0("./outfoldermaps/Selected_LGAs.png"), width=1000, height=1000)
+par(mar=c(0,0,0,0))
+#plotRGB(b.bg, maxpixels=max(500000, 1000*1000), ext=extent(shp.pu), asp=TRUE)
+plot(shp.pu)
+shp.pu.sub <- shp.pu[shp.pu$CADID %in% c(selected_pus),]
+plot(shp.pu.sub, col = "lightslateblue", add = TRUE)
+#suppressWarnings(plot(v.bnd, col="black", lwd=2, add=TRUE))
+dev.off()
 
