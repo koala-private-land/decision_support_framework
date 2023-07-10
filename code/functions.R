5e7d55cf8d91ec825db1f96519aa51757f8faf1e
BrookeWilliams2722 <51690651+BrookeWilliams2722@users.noreply.github.com>
2023-07-10 17:01:26 +1000

    .

--- code/functions.R
+++ code/functions.R
@@ -460,8 +460,6 @@ ld.se <- function(pt){
 }
 
 
-
-
 #####Post processing functions#####
 make.maps <- function(outfoldermaps, scen){
   
@@ -476,7 +474,7 @@ make.maps <- function(outfoldermaps, scen){
   solution <- data.frame(df.to.merge$puid, binary.sol)
   selected_pus_scen1 <- solution$df.to.merge.puid[solution$binary.sol==1]
   
-  png(file=paste0(outfoldermaps, "./", scen, "_bin_budget_", b, ".png"), width=1000, height=1000)
+  png(file=paste0(outfoldermaps, "/", scen, "_bin_budget_", b, ".png"), width=1000, height=1000)
   par(mar=c(0,0,0,0))
   #plotRGB(b.bg, maxpixels=max(500000, 1000*1000), ext=extent(shp.pu), asp=TRUE)
   plot(shp.pu)
@@ -495,10 +493,10 @@ make.maps <- function(outfoldermaps, scen){
   #HEATMAP
   ###For heat map showing selected budget
   ##Join budget values to shp file
-  pu.and.budget <- data.frame(df.to.merge$puid, budget.sol)
-  colnames(pu.and.budget) <- c("CADID", "budget.sol")
+  pu.and.budget <- data.frame(df.to.merge$puid, budget.sol.prop)
+  colnames(pu.and.budget) <- c("CADID", "budget.sol.prop")
   shp.pu.joined <- merge(shp.pu, pu.and.budget, by = "CADID")
-  shp.pu.joined[is.na(shp.pu.joined$budget.sol)] <- 0
+  shp.pu.joined[is.na(shp.pu.joined$budget.sol.prop)] <- 0
   #Export the map
   png(file=paste0(outfoldermaps, "./", scen, "_heatmap_budget_", b, ".png"), width=1000, height=1000)
   par(mar=c(0,0,0,0))
@@ -509,9 +507,185 @@ make.maps <- function(outfoldermaps, scen){
   p <- colorRampPalette(c("white", "#fbb040", "#ef4236"))(128)
   palette(p)
   # Scale the values to the palette
-  vals <- shp.pu.joined$budget.sol
+  vals <- shp.pu.joined$budget.sol.prop
   cols <- (vals - min(vals))/diff(range(vals))*127+1
   plot(shp.pu.joined, col=cols)
+  
+  #Add a legend
+  levels <- unique(shp.pu.joined$budget.sol.prop)
+  levels <- round(levels, digits=1)
+  levels <- sort(levels)
+  col.levels <- unique(cols)
+  col.levels <- sort(col.levels)
+  
+  # add a legend to your map
+  legend("topright",   # location of legend
+         legend = levels, # categories or elements to render in
+         # the legend
+         fill = col.levels, # color palette to use to fill objects in legend.
+         title = "Budget allocated $ AUD",
+         bty = "n",
+         cex = 1.2)
+  cex <- 1
+  scaleBar(shp.pu.sub, pos = "bottomleft",   
+           cex=1,
+           pt.cex = 1.1*cex,
+           seg.len=10*cex,
+           title.cex=cex,
+           outer=FALSE)
+  #suppressWarnings(plot(v.bnd, col="black", lwd=2, add=TRUE))
+  dev.off()
+  
+  
+  #HEATMAP
+  ###For heat map showing selected conservation benefit
+  ##Join budget values to shp file
+  pu.and.consben <- data.frame(df.to.merge$puid, consben.sol)
+  colnames(pu.and.consben) <- c("CADID", "consben.sol")
+  shp.pu.joined <- merge(shp.pu, pu.and.consben, by = "CADID")
+  shp.pu.joined[is.na(shp.pu.joined$consben.sol)] <- 0
+  #Export the map
+  png(file=paste0(outfoldermaps, "./", scen, "_consben_heatmap_", b, ".png"), width=1000, height=1000)
+  par(mar=c(0,0,0,0))
+  #plotRGB(b.bg, maxpixels=max(500000, 1000*1000), ext=extent(shp.pu), asp=TRUE)
+  #plot the polygons without colour
+  plot(shp.pu.joined)
+  # Set the palette
+  p <- colorRampPalette(c("white", "#009933", "#003300"))(128)
+  palette(p)
+  # Scale the values to the palette
+  vals <- shp.pu.joined$consben.sol
+  cols <- (vals - min(vals))/diff(range(vals))*127+1
+  plot(shp.pu.joined, col=cols)
+  
+  #Add a legend
+  levels <- unique(shp.pu.joined$consben.sol)
+  levels <- round(levels, digits=8)
+  levels <- sort(levels)
+  col.levels <- unique(cols)
+  col.levels <- sort(col.levels)
+  
+  # add a legend to your map
+  legend("right",   # location of legend
+         legend = levels, # categories or elements to render in
+         # the legend
+         fill = col.levels, # color palette to use to fill objects in legend.
+         # title = "Suitability weighted koala 
+         # habitat km2",
+         title = "Summed suitability", 
+         bty = "n",
+         cex = 1.2)
+  cex <- 1
+  scaleBar(shp.pu.sub, pos = "bottomleft",   
+           cex=1,
+           pt.cex = 1.1*cex,
+           seg.len=10*cex,
+           title.cex=cex,
+           outer=FALSE)
+  #suppressWarnings(plot(v.bnd, col="black", lwd=2, add=TRUE))
+  dev.off()
+  
+  ###For heat map showing selected conservation benefit/area
+  ##Join budget values to shp file
+  pu.and.consbenarea <- data.frame(df.to.merge$puid, consben.area.sol)
+  colnames(pu.and.consbenarea) <- c("CADID", "consben.area.sol")
+  shp.pu.joined <- merge(shp.pu, pu.and.consbenarea, by = "CADID")
+  shp.pu.joined[is.na(shp.pu.joined$consben.area.sol)] <- 0
+  #Export the map
+  png(file=paste0(outfoldermaps, "./", scen, "_consben_area_heatmap_", b, ".png"), width=1000, height=1000)
+  par(mar=c(0,0,0,0))
+  #plotRGB(b.bg, maxpixels=max(500000, 1000*1000), ext=extent(shp.pu), asp=TRUE)
+  #plot the polygons without colour
+  plot(shp.pu.joined)
+  # Set the palette
+  p <- colorRampPalette(c("white", "#009933", "#003300"))(128)
+  palette(p)
+  # Scale the values to the palette
+  vals <- shp.pu.joined$consben.area.sol
+  cols <- (vals - min(vals))/diff(range(vals))*127+1
+  plot(shp.pu.joined, col=cols)
+  
+  #Add a legend
+  levels <- unique(shp.pu.joined$consben.area.sol)
+  levels <- round(levels, digits=8)
+  levels <- sort(levels)
+  col.levels <- unique(cols)
+  col.levels <- sort(col.levels)
+  
+  # add a legend to your map
+  legend("right",   # location of legend
+         legend = levels, # categories or elements to render in
+         # the legend
+         fill = col.levels, # color palette to use to fill objects in legend.
+         # title = "Suitability weighted koala 
+         # habitat (km2)/area of property (km2)",
+         title = "Summed suitability/
+         area of property (km2)",
+         bty = "n",
+         cex = 1.2)
+  cex <- 1
+  scaleBar(shp.pu.sub, pos = "bottomleft",   
+           cex=1,
+           pt.cex = 1.1*cex,
+           seg.len=10*cex,
+           title.cex=cex,
+           outer=FALSE)
+  #suppressWarnings(plot(v.bnd, col="black", lwd=2, add=TRUE))
+  dev.off()
+  
+}
+make.maps.19areas <- function(outfoldermaps, scen){
+  
+  # create folder:
+  if (!dir.exists(outfoldermaps)){
+    dir.create(outfoldermaps, recursive = TRUE)
+  } else {
+    print("Dir already exists!")
+  }
+  
+  ###For selected or not selected map
+  solution <- data.frame(df.to.merge$puid, binary.sol)
+  selected_pus_scen1 <- solution$df.to.merge.puid[solution$binary.sol==1]
+  
+  png(file=paste0(outfoldermaps, "/", scen, "_bin_budget_", b, ".png"), width=1000, height=1000)
+  par(mar=c(0,0,0,0))
+  #plotRGB(b.bg, maxpixels=max(500000, 1000*1000), ext=extent(shp.pu), asp=TRUE)
+  plot(study_region_outline)
+  plot(shp.pu, add = TRUE)
+  shp.pu.sub <- shp.pu[shp.pu$Invest_PPA %in% c(selected_pus_scen1),]
+  plot(shp.pu.sub, col = "#8a2be2", add = TRUE)
+  cex <- 1
+  scaleBar(shp.pu.sub, pos = "bottomleft",   
+           cex=1,
+           pt.cex = 1.1*cex,
+           seg.len=10*cex,
+           title.cex=cex,
+           outer=FALSE)
+  #suppressWarnings(plot(v.bnd, col="black", lwd=2, add=TRUE))
+  dev.off()
+  
+  #HEATMAP
+  ###For heat map showing selected budget
+  ##Join budget values to shp file
+  pu.and.budget <- data.frame(df.to.merge$puid, budget.sol)
+  colnames(pu.and.budget) <- c("Invest_PPA", "budget.sol")
+  shp.pu.joined <- merge(shp.pu, pu.and.budget, by = "Invest_PPA")
+  shp.pu.joined[is.na(shp.pu.joined$budget.sol)] <- 0
+  #Export the map
+  png(file=paste0(outfoldermaps, "./", scen, "_heatmap_budget_", b, ".png"), width=1000, height=1000)
+  par(mar=c(0,0,0,0))
+  #plotRGB(b.bg, maxpixels=max(500000, 1000*1000), ext=extent(shp.pu), asp=TRUE)
+  #plot the polygons without colour
+  plot(study_region_outline)
+  plot(shp.pu.joined, add = TRUE)
+  # Set the palette
+  p <- colorRampPalette(c("white", "#fbb040", "#ef4236"))(128)
+  palette(p)
+  # Scale the values to the palette
+  vals <- shp.pu.joined$budget.sol
+  cols <- (vals - min(vals))/diff(range(vals))*127+1
+  plot(study_region_outline)
+  plot(shp.pu.joined, col=cols, add = TRUE)
   
   #Add a legend
   levels <- unique(shp.pu.joined$budget.sol)
@@ -696,7 +870,7 @@ exp.vals <- function(outfoldervals, scen){
   solutions.cons.matrix <- solutions*cons.matrix
   cons <- sum(solutions.cons.matrix)
   
-  #Total cons benefit
+  #Total area
   karea.matrix <- matrix(karea.all.incre, nrow=length(unique(pu.df$LGA)))
   solutions.karea.matrix<- solutions*karea.matrix
   karea <- sum(solutions.karea.matrix)
@@ -724,11 +898,108 @@ exp.vals <- function(outfoldervals, scen){
   solutions.se.matrix<- solutions*se.matrix
   s.err <- mean(solutions.se.matrix[solutions.se.matrix>0])
   
+  #Get amount of budget allocated to each planning unit and export it
+  pu.and.budget.to.save <- data.frame(df.to.merge$puid, budget.sol)
+  write.csv(pu.and.budget.to.save, file = paste0(outfoldervals, "./", scen, "_pu_vs_budget_", b, ".csv"), row.names = TRUE)
+  
   #Export the final 
   final.vals <- data.frame(scen = scen, cost = cost, cons.ben = cons, karea = karea, prob.bid = prob, sd = st.dev, se = s.err)
   write.csv(final.vals, file = paste0(outfoldervals, "./", scen, "_budget_", b, ".csv"), row.names = TRUE)
   
 }
+exp.vals.19areas <- function(outfoldervals, scen){
+  
+  # create folder:
+  if (!dir.exists(outfoldervals)){
+    dir.create(outfoldervals, recursive = TRUE)
+  } else {
+    print("Dir already exists!")
+  }
+  
+  #Total cost
+  cost <- sum(budget.sol)
+  
+  #Total cons benefit
+  cons.matrix <- matrix(cons.all.incre, nrow=length(unique(df$puid)))
+  solutions.cons.matrix <- solutions*cons.matrix
+  cons <- sum(solutions.cons.matrix)
+  
+  #Total area
+  karea.matrix <- matrix(karea.all.incre, nrow=length(unique(df$puid)))
+  solutions.karea.matrix<- solutions*karea.matrix
+  karea <- sum(solutions.karea.matrix)
+  
+  #Probability of a bid value
+  #Make a long string of all conservation benefit
+  prob.all.incre <- df_new0$prob.property
+  for (i in 1:y){
+    df.name <- paste0("df_new", i)
+    prob.all.incre <- c(prob.all.incre, get(df.name)$prob.property)
+    prob.all.incre[is.na(prob.all.incre)] = 1000000000000
+  }
+  
+  prob.matrix <- matrix(prob.all.incre, nrow=length(unique(df$puid)))
+  solutions.prob.matrix <- solutions*prob.matrix
+  prob <- sum(solutions.prob.matrix)
+  
+  #Get a measure of SD
+  sd.matrix <- matrix(sd.all.incre, nrow=length(unique(df$puid)))
+  solutions.sd.matrix<- solutions*sd.matrix
+  st.dev <- mean(solutions.sd.matrix[solutions.sd.matrix>0])
+  
+  #Get a measure of SE
+  se.matrix <- matrix(se.all.incre, nrow=length(unique(df$puid)))
+  solutions.se.matrix<- solutions*se.matrix
+  s.err <- mean(solutions.se.matrix[solutions.se.matrix>0])
+  
+  #Get amount of budget allocated to each planning unit and export it
+  pu.and.budget.to.save <- data.frame(df.to.merge$puid, budget.sol)
+  write.csv(pu.and.budget.to.save, file = paste0(outfoldervals, "./", scen, "_pu_vs_budget_", b, ".csv"), row.names = TRUE)
+  
+  #Export the final 
+  final.vals <- data.frame(scen = scen, cost = cost, cons.ben = cons, karea = karea, prob.bid = prob, sd = st.dev, se = s.err)
+  write.csv(final.vals, file = paste0(outfoldervals, "./", scen, "_budget_", b, ".csv"), row.names = TRUE)
+  
+}
+
+
+exp.vals.scen1 <- function(outfoldervals, scen){
+  
+  # create folder:
+  if (!dir.exists(outfoldervals)){
+    dir.create(outfoldervals, recursive = TRUE)
+  } else {
+    print("Dir already exists!")
+  }
+  
+  #Total cost
+  cost <- sum(budget.sol)
+  
+  #Total cons benefit
+  cons <- sum(consben.sol)
+  
+  #Total area
+  karea <- sum(area.sol)
+  
+  #Probability of a bid value
+  prob <- sum(df_final$prob.property)
+  
+  #Get a measure of SD
+  st.dev <- mean(sd.df_scen1[sd.df_scen1>0])
+  
+  #Get a measure of SE
+  s.err <- mean(se.df_scen1[se.df_scen1>0])
+  
+  #Get amount of budget allocated to each planning unit and export it
+  pu.and.budget.to.save <- data.frame(df.to.merge$puid, budget.sol)
+  write.csv(pu.and.budget.to.save, file = paste0(outfoldervals, "./", scen, "_pu_vs_budget_", b, ".csv"), row.names = TRUE)
+  
+  #Export the final 
+  final.vals <- data.frame(scen = scen, cost = cost, cons.ben = cons, karea = karea, prob.bid = prob, sd = st.dev, se = s.err)
+  write.csv(final.vals, file = paste0(outfoldervals, "./", scen, "_budget_", b, ".csv"), row.names = TRUE)
+  
+}
+
 
 
 ###Code to get values
